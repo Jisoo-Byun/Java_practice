124160c3a47a0ce6b5070deace48650397691df2
byunjisoo <bjs1616@gmail.com>
2019-12-19 21:53:28 +0900

    입출력 공부 Scanner 사용

---
+++ chap01/src/sec03/exam01/ScannerDemo.java
@@ -0,0 +1,14 @@
+package sec03.exam01;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		System.out.println(i*100);
+		sc.close();	
+	}
+}
--- chap01/src/sec03/exam01/inputforeachdemo.java
+++ chap01/src/sec03/exam01/inputforeachdemo.java
@@ -1,10 +1,14 @@
 package sec03.exam01;
 
-public class Hello {
+
+
+public class inputforeachdemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println(args.length);
+		for(String e : args) {
+			System.out.println(e);
+		}
 	}
 
 }
